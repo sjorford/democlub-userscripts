e92098b7e236291339108d684916af880edf068f
sjorford <sjorford@gmail.com>
2018-02-15 10:29:58 +0000

    rename

--- democlub-recent-changes.user.js
+++ democlub-recent-changes.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name           Demo Club recent changes
+// @name           Democracy Club recent changes
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        2018.02.13
+// @version        2018.02.15
 // @match          https://candidates.democracyclub.org.uk/recent-changes*
 // @grant          none
 // @require        https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
