544706537b49d4d1c7555ad1e7a0ef8e1ad16b55
sjorford <sjorford@gmail.com>
2018-02-28 13:50:56 +0000

    colour districts by type

--- democlub-select-election.user.js
+++ democlub-select-election.user.js
@@ -2,11 +2,12 @@
 // @name           Democracy Club select election
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        2018.02.26.2
+// @version        2018.02.28
 // @match          https://candidates.democracyclub.org.uk/person/create/select_election?*
 // @grant          none
 // @require        https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
 // @require        https://raw.githubusercontent.com/sjorford/democlub-userscripts/master/lib/utils.js
+// @require        https://raw.githubusercontent.com/sjorford/democlub-userscripts/master/lib/districts.js
 // ==/UserScript==
 
 // TODO:
@@ -25,13 +26,16 @@ function onready() {
 	
 	$(`<style>
 		
-		.sjo-addperson-listcolumns {column-width: 200px; -moz-column-width: 200px;}
-		.sjo-addperson-listcolumns p {font-size: 0.8rem}
+		.sjo-addperson-listcolumns {column-width: 200px;}
+		.sjo-addperson-listcolumns p {font-size: 0.8rem;}
 		.sjo-addperson-listitem {margin: 0; padding-left: 3.05em; text-indent: -3.05em;}
-		.sjo-addperson-button {margin: 0 0 0.25em 0; padding: 0.25em 0.5em; text-indent: 0; font-size: 0.8rem}
-		.sjo-addperson-text {color: inherit;}
-		.sjo-addperson-latest .sjo-addperson-button {background-color: #fc0 !important;}
-		.sjo-addperson-latest .sjo-addperson-text {font-weight: bold;}
+		
+		.sjo-addperson-button {margin: 0 0 0.25em 0; padding: 0.25em 0.5em; text-indent: 0; font-size: 0.8rem; text-align: left; width: 100%; background-color: #e7e7e7; color: black;}
+		.sjo-addperson-button-LBO {background-color: #c4d1ff;}
+		.sjo-addperson-button-MBO {background-color: #ffbaba;}
+		.sjo-addperson-button-UTA {background-color: #ffe766;}
+		.sjo-addperson-button-CTY {background-color: #6fd16f;}
+		.sjo-addperson-button-DIS {background-color: #95f095;}
 		
 		.sjo-filter {display: inline-block !important; width: 15em !important; padding: 0.1rem !important; height: 1.5rem !important;}
 		.sjo-hidden {display: none;}
@@ -49,18 +53,19 @@ function onready() {
 			// Parse the button text
 			var listitem = $(this).addClass('sjo-addperson-listitem');
 			var button = $('a', listitem).addClass('sjo-addperson-button');
-			
-			// Move the election name out of the button
 			var electionName = button.text().trim().match(/^Add .+? to the (.*?)( (local|mayoral) election)?$/)[1];
 			electionName = Utils.shortOrgName(electionName);
-			button.text('Add').after(` <a class="sjo-addperson-text" href="${button.attr('href')}">${electionName}</a>`);
-			
+			button.text(electionName);
+						
 			// Add an ID to the button
 			var electionID = button.attr('href').match(/\/election\/(.*)\/person\//)[1];
-			if (!listitem.attr('id')) listitem.attr('id', 'sjo-addperson-listitem-' + electionID.replace(/\./g, '_'));
+			if (!button.attr('id')) button.attr('id', 'sjo-addperson-button-' + electionID.replace(/\./g, '_'));
 			
-			// Flag elections by country
-			//listitem.addClass('sjo-addperson-listitem-' + Utils.countryForElection(electionID));
+			// Flag districts by type
+			var district = Districts[electionName];
+			if (district) {
+				button.addClass('sjo-addperson-button-' + district.type);
+			}
 			
 		});
 		
@@ -75,8 +80,8 @@ function onready() {
 	
 	// Get all unique election groups
 	var groups = [];
-	$('.sjo-addperson-listitem').each((index, element) => {
-		var match = $(element).attr('id').match(/^sjo-addperson-listitem-(.*?)_(.*_)?(\d{4}-\d{2}-\d{2})$/);
+	$('.sjo-addperson-button').each((index, element) => {
+		var match = $(element).attr('id').match(/^sjo-addperson-button-(.*?)_(.*_)?(\d{4}-\d{2}-\d{2})$/);
 		var group = match[3] + '_' + match[1];
 		if (groups.indexOf(group) < 0) groups.push(group);
 	});
@@ -87,15 +92,16 @@ function onready() {
 	// TODO: display local/mayor/etc. subheadings, but not always?
 	$.each(groups, (index, group) => {
 		var parts = group.split('_');
-		var listitems = $(`[id^="sjo-addperson-listitem-${parts[1]}"][id\$="${parts[0]}"]`);
+		var listitems = $(`[id^="sjo-addperson-button-${parts[1]}"][id\$="${parts[0]}"]`).closest('.sjo-addperson-listitem');
 		$('<div role="list"></div>').appendTo('.sjo-addperson-listcolumns').append(listitems).before(`<h4>${moment(parts[0], "YYYY-MM-DD").format("D MMM YYYY")}</h4>`);
 	});
 	
 	// Store button ID when clicked
-	$('body').on('click', '.sjo-addperson-listitem', event => localStorage.setItem('sjo-addperson-button', $(event.target).closest('.sjo-addperson-listitem').attr('id')));
+	$('body').on('click', '.sjo-addperson-button', event => localStorage.setItem('sjo-addperson-button', $(event.target).attr('id')));
 	
 	// Retrieve button ID on load
 	var lastButtonID = localStorage.getItem('sjo-addperson-button');
+	console.log(lastButtonID);
 	if (lastButtonID) $(`[id="${lastButtonID}"]`).addClass('sjo-addperson-latest');
 	
 	// Add filter
@@ -107,8 +113,8 @@ function onready() {
 		console.log(filterText);
 		
 		$('.sjo-addperson-listitem').each((index, element) => {
-			var label = $(element);
-			label.toggleClass('sjo-hidden', !label.find('.sjo-addperson-text').text().trim().toLowerCase().match(filterText));
+			var listitem = $(element);
+			listitem.toggleClass('sjo-hidden', !listitem.text().trim().toLowerCase().match(filterText));
 		});
 		
 		$('.sjo-addperson-listcolumns h4').each((index, element) => {
