4ba9626824a2bc89328edceab880fa62ccbc1e2b
sjorford <sjorford@gmail.com>
2018-03-07 13:55:08 +0000

    format mayoral elections

--- democlub-candidate.user.js
+++ democlub-candidate.user.js
@@ -2,7 +2,7 @@
 // @name        Democracy Club candidate
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/person/*
-// @version     2018.03.05.0
+// @version     2018.03.07.0
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
 // @require     https://raw.githubusercontent.com/sjorford/democlub-userscripts/master/lib/utils.js
@@ -50,7 +50,12 @@ function onready() {
 			
 			// Format election headers
 			var date = link.attr('href').match(/(\d{4}(-\d{2}-\d{2})?)\/post\//)[1];
-			var council = Utils.shortOrgName(dt.html().trim().replace(/^Contest(ed|ing) the (\d{4} )?| local election$/g, ''));
+			var headingText = dt.html().trim();
+			var council = Utils.shortOrgName(headingText.replace(/^Contest(ed|ing) the (\d{4} )?| (local|mayoral) election$/g, ''));
+			if (headingText.match(/ mayoral election$/)) {
+				link.text(Utils.shortOrgName(link.text()));
+				council = 'Mayor of ' + council;
+			}
 			dt.html(council + ' <span class="sjo-heading-note">(' + (date.length > 4 ? moment(date).format("D MMM YYYY") : date) + ')</span>');
 			
 			// Remove duplicate votes
