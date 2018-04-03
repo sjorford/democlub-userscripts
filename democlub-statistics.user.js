9cb4957099ea682bac28d5feb59037a5bebb34d6
sjorford <sjorford@gmail.com>
2018-04-03 07:44:29 +0100

    link elections

--- democlub-statistics.user.js
+++ democlub-statistics.user.js
@@ -2,7 +2,7 @@
 // @name        Democracy Club statistics
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/numbers/
-// @version     2018.02.15
+// @version     2018.04.03.0
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
 // @require     https://raw.githubusercontent.com/sjorford/js/master/sjo-jq.js
@@ -63,8 +63,8 @@ function onready() {
 				
 				var row = $('<tr></tr>')
 					.addCell(date)
-					.addCell(key)
-					.addCell(area);
+					.addCellHTML(`<a href="/election/${key}.${date}/constituencies">${area}</a>`)
+					.addCell(key);
 				
 				if (date != lastDate) {
 					row.addClass('sjo-stats-break');
