e85d130b9ad34930d98d0c56d5410a2774aff04c
sjorford <sjorford@gmail.com>
2018-04-18 15:12:38 +0100

    embed PDF directly to avoid Google 204 errors

---
+++ democlub-document-viewer.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name           Democracy Club document viewer
+// @namespace      sjorford@gmail.com
+// @author         Stuart Orford
+// @version        2018.04.18.0
+// @match          https://candidates.democracyclub.org.uk/upload_document/*
+// @grant          none
+// ==/UserScript==
+
+// https://stackoverflow.com/questions/35298724/google-docs-viewer-occasionally-failing-to-load-content-in-iframe/46095583#46095583
+
+$(function() {
+	
+	$('<style>.sjo-docviewer {width: 100%; height: 600px;}</style>').appendTo('head');
+	
+	var iframe = $('.document_viewer');
+	var viewerUrl = iframe.attr('src');
+	var match = viewerUrl.match(/^https:\/\/docs\.google\.com\/viewer\?url=(.*)&embedded=true$/);
+	if (!match) return;
+	
+	var url = decodeURIComponent(match[1]);
+	var obj = $(`
+		<object class="sjo-docviewer" data="${url}" type="application/pdf">
+			<embed src="${url}" type="application/pdf"></embed>
+		</object>`);
+	iframe.after(obj).remove();
+	
+});
