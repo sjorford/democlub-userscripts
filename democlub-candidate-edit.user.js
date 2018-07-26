dac808ad9946ec66f1f908832110da3734a2bb4a
sjorford <sjorford@gmail.com>
2018-07-26 14:29:15 +0100

    +url

--- democlub-candidate-edit.user.js
+++ democlub-candidate-edit.user.js
@@ -2,9 +2,10 @@
 // @name        Democracy Club candidate edit
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/person/*/update
+// @include     https://candidates.democracyclub.org.uk/person/*/update/
 // @include     https://candidates.democracyclub.org.uk/person/*/update?highlight_field=*
 // @include     https://candidates.democracyclub.org.uk/election/*/person/create/*
-// @version     2018.05.22.0
+// @version     2018.07.26.0
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
 // @require     https://raw.githubusercontent.com/sjorford/democlub-userscripts/master/lib/utils.js
