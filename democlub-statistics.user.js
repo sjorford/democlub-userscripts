087169a27430541757064427ec46736c845bac2e
sjorford <sjorford@gmail.com>
2018-08-27 07:42:14 +0100

    hide only May elections

--- democlub-statistics.user.js
+++ democlub-statistics.user.js
@@ -2,7 +2,7 @@
 // @name        Democracy Club statistics
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/numbers/
-// @version     2018.06.01.0
+// @version     2018.08.27.0
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
 // @require     https://raw.githubusercontent.com/sjorford/js/master/sjo-jq.js
@@ -27,7 +27,8 @@ function onready() {
 	</style>`).appendTo('head');
 	
 	var futureTable = $('<table class="sjo-stats"></table>');
-	var cutoff = moment().add(30, 'days');
+	var cutoffDate = moment().add(30, 'days');
+	var mayDate = moment('2019-05-02');
 	
 	$('.statistics-elections').each(function(index, element) {
 		var wrapper = $(element);
@@ -88,7 +89,7 @@ function onready() {
 					}
 				});
 				
-				if (moment(date).isAfter(cutoff)) {
+				if (moment(date).isAfter(cutoffDate) && moment(date).isSameOrAfter(mayDate)) {
 					row.appendTo(futureTable);
 				} else {
 					row.appendTo(table);
