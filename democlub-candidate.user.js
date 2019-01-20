a95e30df8102e8d95c50c2454b647764526698d0
sjorford <sjorford@gmail.com>
2019-01-20 06:39:06 +0000

    update biscuit label

--- democlub-candidate.user.js
+++ democlub-candidate.user.js
@@ -2,7 +2,7 @@
 // @name        Democracy Club candidate
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/person/*
-// @version     2019.01.20.2
+// @version     2019.01.20.3
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
 // @require     https://raw.githubusercontent.com/sjorford/democlub-userscripts/master/lib/utils.js
@@ -36,6 +36,7 @@ function onready() {
 		'Wikipedia URL': 								'Wikipedia',
 		'LinkedIn URL': 								'LinkedIn',
 		"The party's candidate page for this person": 	'Party page',
+		'Favourite biscuit':							'Biscuit',
 		
 		'Twitter_Username':								'Twitter',
 		'Facebook_Personal_Url':						'FB profile',
@@ -45,7 +46,7 @@ function onready() {
 		'Linkedin_Url':									'LinkedIn',
 		'Party_Ppc_Page_Url':							'Party page',
 	};
-	labelMappings[`Favourite biscuit ${Unicode.COOKIE}`] = `Biscuit ${Unicode.COOKIE}`;
+	//labelMappings[`Favourite biscuit ${Unicode.COOKIE}`] = `Biscuit ${Unicode.COOKIE}`;
 	
 	// Candidate details
 	$('dt', '.person__details, .person__versions').each((index, element) => {
