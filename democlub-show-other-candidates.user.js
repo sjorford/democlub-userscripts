058e083b47d517af18d94d86841a28bb769be61d
sjorford <sjorford@gmail.com>
2019-02-24 09:20:54 +0000

    position next to election section

--- democlub-show-other-candidates.user.js
+++ democlub-show-other-candidates.user.js
@@ -3,12 +3,13 @@
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/person/*/update
 // @include     https://candidates.democracyclub.org.uk/election/*/person/create/*
-// @version     2018.02.27
+// @version     2019.02.24.0
 // @grant       none
 // @require     https://raw.githubusercontent.com/sjorford/democlub-userscripts/master/lib/utils.js
 // ==/UserScript==
 
 // TODO: also handle inline form on https://candidates.democracyclub.org.uk/election/*/post/*
+// TODO: does this handle multiple current elections?
 
 // temporary fix due to c.dc script errors
 // $(onready);
@@ -17,7 +18,7 @@ window.setTimeout(onready, 0);
 function onready() {
 	
 	$(`<style>
-		.sjo-post-candidates {background-color: #ff9; margin-top: 25em;}
+		.sjo-post-candidates {background-color: #ff9;}
 		.sjo-post-candidates p {margin-bottom: 0.25em;}
 		.sjo-is-current {font-weight: bold;}
 	</style>`).appendTo('head');
@@ -42,9 +43,14 @@ function onready() {
 	
 	function renderPostCandidates(electionID, postID, postName, data) {
 		
-		var block = $('#sjo-post-candidates-' + electionID.replace(/\./g, '_'));
+		var blockID = `#sjo-post-candidates-${electionID.replace(/\./g, '_')}`;
+		var block = $(blockID);
 		if (block.length === 0) {
-			block = $('<div class="person__actions__action sjo-post-candidates" id="sjo-post-candidates-' + electionID.replace(/\./g, '_') + '"></div>').appendTo('.person__actions');
+			block = $(`<div class="person__actions__action sjo-post-candidates" id="${blockID}"></div>`).appendTo('.person__actions');
+			var electionsTop = $('#add_election_button').closest('div:has(h2)').offset().top;
+			if (block.offset().top < electionsTop) {
+				block.offset({top: electionsTop});
+			}
 		}
 		block.append(`<h2>Current candidates for ${postName}</h2>`);
 		
