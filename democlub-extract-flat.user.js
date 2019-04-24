75f72b0c71536dbc1ec1b7e53c3a0bf5e3faf7f9
sjorford <sjorford@gmail.com>
2019-04-24 18:02:24 +0100

    flattened extract for merge finding

---
+++ democlub-extract-flat.user.js
@@ -0,0 +1,88 @@
+// ==UserScript==
+// @name           Democracy Club extract flat
+// @namespace      sjorford@gmail.com
+// @author         Stuart Orford
+// @version        2019.04.22.0
+// @match          https://candidates.democracyclub.org.uk/help/api
+// @grant          none
+// ==/UserScript==
+
+$(function() {
+	
+	var columns = [
+		'id',
+		'name',
+		'_first_name',
+		'_last_name',
+		'email',
+		'twitter_username',
+		'facebook_page_url',
+		'party_ppc_page_url',
+		'facebook_personal_url',
+		'homepage_url',
+		'wikipedia_url',
+		'linkedin_url',
+	];
+	
+	var repeatColumns = [
+		'election',
+		'post_label',
+		'party_name',
+	];
+	
+	$('body').on('sjo-api-loaded', event => {
+		
+		$('<input type="button" value="Flat">').insertAfter('#sjo-api-button-download').click(event => {
+			
+			var rawData = sjo.api.tableData;
+			var flatData = {};
+			
+			$.each(rawData, (index, candObj) => {
+				if (!flatData[candObj.id]) {
+					flatData[candObj.id] = [candObj];
+				} else {
+					flatData[candObj.id].push(candObj);
+				}
+			});
+			
+			var html = buildRawOutput(flatData);
+			
+			$('#sjo-api-textarea-raw').html(html.join('\r\n')).show();
+
+		});
+		
+	});
+	
+	function buildRawOutput(data) {
+		
+		var bodyHtml = [];
+		
+		$.each(data, function(index, dataRow) {
+			bodyHtml.push(buildRawOutputRow(dataRow).join('\t'));
+		});
+		
+		return bodyHtml;
+		
+	}
+	
+	function buildRawOutputRow(dataRow) {
+		
+		var cellValues = columns.map(column => {
+			if (dataRow[0][column] === null || dataRow[0][column] === false || dataRow[0][column] === '') {
+				return '';
+			} else {
+				return typeof dataRow[0][column] == 'string' ? dataRow[0][column].replace(/\s+/g, ' ') : dataRow[0][column];
+			}
+		});
+		
+		$.each(dataRow, (index, candObj) => {
+			$.each(repeatColumns, (index, column) => {
+				cellValues.push(candObj[column]);
+			});
+		});
+		
+		return cellValues;
+		
+	}
+	
+});
