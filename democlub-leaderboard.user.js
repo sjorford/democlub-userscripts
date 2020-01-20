bfc327212ac2287ea6f95166521fdfa793747939
sjorford <sjorford@gmail.com>
2020-01-20 18:06:33 +0000

    long names

--- democlub-leaderboard.user.js
+++ democlub-leaderboard.user.js
@@ -2,7 +2,7 @@
 // @name           Democracy Club leaderboard
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        2019.11.22.0
+// @version        2020.01.20.0
 // @match          https://candidates.democracyclub.org.uk/leaderboard
 // @grant          none
 // ==/UserScript==
@@ -11,12 +11,14 @@ $(function() {
 	
 	$(`<style>
 		.leaderboard {width: auto !important; margin-right: 2em;}
+		.leaderboard td {max-width: 200px !important;}
 	</style>`).appendTo('head');
+	
+	$('.leaderboard th').filter((i,e) => $(e).text().trim() == 'Number of edits').text('Edits');
 	
 	$('.leaderboard td:first-of-type').each((i,e) => {
 		var cell = $(e);
 		var username = cell.text().trim();
-		var bot = 
 		cell.html(`<a href="/recent-changes?username=${username}">${username + (username.match(/Bot$/) ? ' 🤖' : '')}</a>`)
 	});
 	
