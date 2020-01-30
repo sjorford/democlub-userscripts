73f645a762c14bb0eeedc7720e52c29479581cd9
sjorford <sjorford@gmail.com>
2020-01-30 16:17:11 +0000

    100% faster
    it turns out the HTML parser is a *lot* faster than DOM manipulation

--- democlub-statistics.user.js
+++ democlub-statistics.user.js
@@ -2,7 +2,8 @@
 // @name        Democracy Club statistics
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/numbers/
-// @version     2019.07.10.1
+// @version     2020.01.30.0
+// @isitfast    yes
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
 // @require     https://raw.githubusercontent.com/sjorford/js/master/sjo-jq.js
@@ -27,117 +28,55 @@ function onready() {
 		.sjo-filter {display: inline-block;}
 	</style>`).appendTo('head');
 	
-	var futureTable = $('<table class="sjo-stats"></table>');
-	var cutoffDate = moment().add(30, 'days');
-	var mayDate = moment('2019-05-02');
-	
+	// New version - produce raw HTML for speed
 	$('.statistics-elections').each(function(index, element) {
-		var wrapper = $(element);
 		
-		var table = $('<table class="sjo-stats"></table>')
-			.insertAfter(wrapper.find('h2'));
-		
-		if (wrapper.hasClass('current')) {
-			futureTable.insertAfter(wrapper).wrap('<div class="statistics-elections sjo-future">').before('<h2>Future Elections </h2>');
-		}
-		
+		var html = element.innerHTML;
+		var htmlSplit = html.match(/([\s\S]+)(<h3>[\s\S]+)/);
+		var blocks = htmlSplit[1].replace(/(<h3>)/g, '🤒$1').split('🤒');
 		var lastDate = null;
 		
-		$('div', wrapper).each(function(index, element) {
+		for (var blockIndex = 1; blockIndex < blocks.length; blockIndex++) {
 			
-			var div = $(element);
-			
-			var id = div.attr('id');
-			id = id == 'statistics-election-2010' ? 'statistics-election-parl-2010-05-06' : id == 'statistics-election-2015' ? 'statistics-election-parl-2015-05-07' : id;
-			
-			var matchId = id.match(/^statistics-election-((parl|sp|naw|nia|gla|mayor|pcc|local|europarl)(-(a|r|c))?(-([-a-z]{2,}))?)-(\d{4}-\d{2}-\d{2})$/);
-			
-			var headerText = div.find('h4').text();
-			var matchHeader = headerText.match(/^Statistics for the (\d{4} )?(.+?)( (local|[Mm]ayoral))?( [Ee]lection|by-election: (.*) (ward|constituency))?( \((.+)\))?$/, '');
-			
-			if (matchId && matchHeader) {
+			var bits = blocks[blockIndex].replace(/(<h4>[\s\S]*?<div[\s\S]*?<h4>)/g, '😤$1').split('😤');
+			for (var bitIndex = 1; bitIndex < bits.length; bitIndex++) {
+				
+				var bitMatch = bits[bitIndex].match(/<div id="(.*?)">\s*<h4>(.*?)<\/h4>\s*<ul>\s*<li>Total candidates: (\d+)<\/li>([\s\S]*?)<\/ul>/);
+
+				var id = bitMatch[1];
+				var matchId = id.match(/^statistics-election-((parl|sp|naw|nia|gla|mayor|pcc|local|europarl)(-(a|r|c))?(-([-a-z]{2,}))?)-(\d{4}-\d{2}-\d{2})$/);
+				
+				var headerText = bitMatch[2];
+				var matchHeader = headerText.match(/^Statistics for the (\d{4} )?(.+?)( (local|[Mm]ayoral))?( [Ee]lection|by-election: (.*) (ward|constituency))?( \((.+)\))?$/, '');
 				
 				var key = matchId[2] + (matchId[3] ? '.' + matchId[4] : '') + (matchId[5] ? '.' + matchId[6] : '');
 				var type = matchId[2];
 				var date = matchId[7];
 				var area = Utils.shortOrgName(matchHeader[2]) + (matchHeader[8] ? matchHeader[8] : '');
 				
-				var bullets = div.find('li');
+				var num = bitMatch[3];
+				var items = bitMatch[4]
+								.replace(/<(\/)?li>/g, '<$1td class="sjo-nowrap">')
+								.replace(/Candidates per/g, 'by')
+								.replace(/See progress towards locking all posts/, 'progress');
 				
-				var row = $('<tr></tr>')
-					.addCell(date)
-					.addCellHTML(`<a href="/election/${key}.${date}/constituencies">${area}</a>`)
-					.addCell(key);
+				bits[bitIndex] = `
+<tr${date != lastDate ? ' class="sjo-stats-break"' : ''}>
+<td>${date}</td>
+<td><a href="/election/${key}.${date}/constituencies">${area}</a></td>
+<td>${key}</td>
+<td class="sjo-number${num == '0' ? ' sjo-number-zero' : ''}">${num}</td>
+${items}
+</tr>`;
 				
-				if (date != lastDate) {
-					row.addClass('sjo-stats-break');
-					lastDate = date;
-				}
-				
-				bullets.each(function(index, element) {
-					var bullet = $(element);
-					var link = bullet.find('a');
-					if (link.length > 0) {
-						link.html(link.html().trim()
-							.replace(/^Candidates per /, 'by ')
-							.replace(/^See progress towards locking all posts$/, 'progress'));
-						$('<td class="sjo-nowrap"></td>').append(link).appendTo(row);
-						bullet.addClass('sjo-remove');
-					} else if (bullet.text().indexOf(':') >= 0) {
-						var num = bullet.text().split(':')[1].trim();
-						row.addCell(num, 'sjo-number' + (num == '0' ? ' sjo-number-zero' : ''));
-						bullet.addClass('sjo-remove');
-					}
-				});
-				
-				if (moment(date).isAfter(cutoffDate) && moment(date).isSameOrAfter(mayDate)) {
-					row.appendTo(futureTable);
-				} else {
-					row.appendTo(table);
-				}
-				
-				$('.sjo-remove').remove();
-				if (div.find('li').length === 0) {
-					div.remove();
-				}
-				
+				lastDate = date;
 			}
 			
-		});
-		
-		$('h3, h4', wrapper).filter((index, element) => {
-			var heading = $(element);
-			return heading.next().length == 0 || heading.next('h3, h4').length > 0;
-		}).remove();
-		
-	});
-	
-	$('.statistics-elections').each((index, element) => {
-		var wrapper = $(element);
-		var table = wrapper.find('.sjo-stats');
-		if (table.find('tr').length == 0) {
-			wrapper.hide();
-		} else {
-			table.collapsible(wrapper.find('h2'), wrapper.is('.current, .sjo-future'));
+			blocks[blockIndex] = bits.slice(1).join('');
 		}
+		
+		html = '<table class="sjo-stats">' + blocks.join('') + '</table>';
+		element.innerHTML = html;
 	});
-	
-	$('body').on('click', '.sjo-stats', event => {
-		if (event.target.tagName != 'A') {
-			$(event.target).closest('.sjo-stats').selectRange();
-		}
-	});
-	
-	// Add a filter
-	var filter = $('<input type="text" class="sjo-filter">')
-		.insertBefore('.statistics-elections.current').wrap('<div></div>').before('Filter elections:')
-		.on('change keyup', applyFilter);
-	
-	function applyFilter() {
-		var text = filter.val().toLowerCase().trim();
-		$('.sjo-stats').find('tr').hide()
-			.filter((index, element) => $(element).find('td:lt(3)').text().toLowerCase().indexOf(text) >= 0).show()
-			.closest('.sjo-stats').expand();
-	}
 	
 }
