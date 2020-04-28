8e31d862541cb8d2275dd65921ad176db19dace6
sjorford <sjorford@gmail.com>
2020-04-28 12:23:31 +0100

    are you sure?

--- democlub-merge-check.user.js
+++ democlub-merge-check.user.js
@@ -3,7 +3,7 @@
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/person/*/update
 // @include     https://candidates.democracyclub.org.uk/person/*/update/
-// @version     2020.04.28.0
+// @version     2020.04.28.1
 // @grant       none
 // ==/UserScript==
 
@@ -11,19 +11,28 @@ $(function() {
 	
 	var mergeButton = $('#person-merge [type="submit"]').prop('disabled', true);
 	var wrapper = $('<div></div>').insertAfter(mergeButton);
-	var thisName = $('.person__hero h1 a').text();
+	var thisName = $('.person__hero h1').text().replace(/^Editing: /, '');
 	var otherID;
 	var inputOtherID = $('#other').on('change, keyup', event => {
 		if (inputOtherID.val() == otherID) return;
+		
 		otherID = inputOtherID.val();
 		mergeButton.prop('disabled', true);
 		wrapper.text('Checking...');
 		$.get(`/person/${otherID}`, (data, status, jqxhr) => {
+			
 			var otherName = $(data).find('.person__hero h1').text();
 			wrapper.text('Merging with ' + otherName);
-			console.log(thisName, otherName);
-			if (otherName == thisName) mergeButton.removeProp('disabled');
+			if (otherName == thisName) {
+				mergeButton.removeProp('disabled');
+			} else {
+				wrapper.append('<div>Are you sure?<div><input type="button" class="button sjo-yesimsure" value="Yes"></div></div>')
+			}
+			
+			$('.sjo-yesimsure').click(event => mergeButton.removeProp('disabled'));
+			
 		}).fail((jqxhr, status, error) => wrapper.text(status + ': ' + error));
+		
 	});
 	
 });
