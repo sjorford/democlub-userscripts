88a3f4bb7d18be517e580a947406bf0dc6d1dbed
sjorford <sjorford@gmail.com>
2020-11-13 06:01:03 +0000

    -utm_source

--- democlub-candidate-edit.user.js
+++ democlub-candidate-edit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Democracy Club candidate edit
 // @namespace   sjorford@gmail.com
-// @version     2020.10.30.2
+// @version     2020.11.13.0
 // @include     https://candidates.democracyclub.org.uk/person/*/update
 // @include     https://candidates.democracyclub.org.uk/person/*/update/
 // @include     https://candidates.democracyclub.org.uk/person/*/update?highlight_field=*
@@ -264,7 +264,7 @@ function onready() {
 			var href = input.val().trim();
 			
 			// Strip tracking links
-			href = href.replace(/\?(originalSubdomain|fbclid|ref)=.*/, '');
+			href = href.replace(/\?(originalSubdomain|fbclid|ref|utm_source)=.*/, '');
 			
 			input.val(href).removeClass('sjo-input-invalid');
 			
