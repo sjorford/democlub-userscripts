951c6972bf1a7dcbc8c650a1673c4dfa887bc863
Stuart Orford <sjorford@gmail.com>
2020-12-07 12:34:41 +0000

    hl=en

--- democlub-candidate-edit.user.js
+++ democlub-candidate-edit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Democracy Club candidate edit
 // @namespace   sjorford@gmail.com
-// @version     2020.11.22.0
+// @version     2020.12.07.0
 // @include     https://candidates.democracyclub.org.uk/person/*/update
 // @include     https://candidates.democracyclub.org.uk/person/*/update/
 // @include     https://candidates.democracyclub.org.uk/person/*/update?highlight_field=*
@@ -266,7 +266,7 @@ function onready() {
 			var href = input.val().trim();
 			
 			// Strip tracking links
-			href = href.replace(/\?(originalSubdomain|fbclid|ref|utm_source)=.*/, '');
+			href = href.replace(/\?(originalSubdomain|fbclid|ref|utm_source|hl)=.*/, '');
 			
 			input.val(href).removeClass('sjo-input-invalid');
 			
