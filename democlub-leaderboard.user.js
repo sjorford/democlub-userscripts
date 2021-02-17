b3ccc93c5e613e98eec70cc7addc96bff8b2aaf2
Stuart Orford <sjorford@gmail.com>
2021-02-17 15:37:08 +0000

    formatting

--- democlub-leaderboard.user.js
+++ democlub-leaderboard.user.js
@@ -2,7 +2,7 @@
 // @name           Democracy Club leaderboard
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        2020.01.20.0
+// @version        2021.02.17.0
 // @match          https://candidates.democracyclub.org.uk/leaderboard
 // @grant          none
 // ==/UserScript==
@@ -10,8 +10,9 @@
 $(function() {
 	
 	$(`<style>
-		.leaderboard {width: auto !important; margin-right: 2em;}
+		.leaderboard {width: auto !important; margin-right: 2em; float: left;}
 		.leaderboard td {max-width: 200px !important;}
+		.leaderboard td:last-of-type {text-align: right;}
 	</style>`).appendTo('head');
 	
 	$('.leaderboard th').filter((i,e) => $(e).text().trim() == 'Number of edits').text('Edits');
