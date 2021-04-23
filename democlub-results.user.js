6f4fd7bc2fe831811fe735659c8b08a9b5885b7e
sjorford <sjorford@gmail.com>
2021-04-23 07:46:25 +0100

    remove old code

--- democlub-results.user.js
+++ democlub-results.user.js
@@ -2,7 +2,7 @@
 // @name           Democracy Club results
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        2021.04.23.0
+// @version        2021.04.23.1
 // @match          https://candidates.democracyclub.org.uk/uk_results/*
 // @grant          none
 // @require        https://raw.githubusercontent.com/sjorford/democlub-userscripts/master/lib/utils.js
@@ -47,56 +47,6 @@ $(function() {
 		.each((index, element) => element.type = 'text')
 		.css({marginBottom: '0'})
 		.first().focus();
-	
-	$('<textarea placeholder="Paste results table here"></textarea>').insertBefore(form).on('paste', event => setTimeout(() => {
-		
-		var inputs = firstBody.find('input');
-		var data = event.target.value.split('\n').map(a => a.split('\t').map(b => b.trim()));
-		var fieldsFound = 0, numbersFound = 0;
-		$.each(data, (rowIndex, rowValues) => {
-			
-			var nameValues = $.grep(rowValues, value => value.match(/[A-Z]{3}/i));
-			var numberValues = $.grep(rowValues, value => value.match(/\d{1,3}(,?\d{3})?/));
-			if (nameValues.length > 0 && numberValues.length > 0) {
-				numbersFound++;
-				
-				var nameMatch = nameValues[0].match(/^([^,]+)(,(.*?))?$/);
-				var name = ((nameMatch[3] || '') + ' ' + nameMatch[1]).trim();
-				var firstName = name.match(/^[^\s]+/)[0].toLowerCase().trim();
-				var lastName = name.match(/[^\s]+$/)[0].toLowerCase().trim();
-				console.log(name, firstName, lastName, numberValues[0]);
-				
-				inputs.each((inputIndex, element) => {
-					var input = $(element);
-					if (!input.val()) {
-						if (input.closest('tr').text().toLowerCase().indexOf(firstName) >= 0
-						   && input.closest('tr').text().toLowerCase().indexOf(lastName) >= 0) {
-							input.val(numberValues[0].replace(/[^\d]/g, ''));
-							fieldsFound++;
-							return false;
-						}
-					}
-				});
-				
-			}
-			
-		});
-		
-		if (fieldsFound != inputs.length || numbersFound != inputs.length) 
-			alert (fieldsFound + ' != ' + numbersFound + ' != ' + inputs.length);
-		
-	}, 0));
-	
-	/*
-	var prevSource = localStorage.getItem('sjo-result-source');
-	if (prevSource) {
-		var sourceWrapper = $(`<div>Previous source: <a href="${prevSource}" target="_blank">${prevSource}</a></div>`)
-			.insertAfter(form.find('table'));
-		$('<button type="button">Use same source</button>').appendTo(sourceWrapper).wrap('<div></div>').click(event => $('#id_source').val(prevSource));
-	}
-	
-	$('body').on('submit', event => localStorage.setItem('sjo-result-source', $('#id_source').val()));
-	*/
 	
 	// Format sources
 	$('caption q').each((index, element) => {
