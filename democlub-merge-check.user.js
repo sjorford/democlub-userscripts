7ec2c226b096456b55145a567b37f86761e507f4
sjorford <sjorford@gmail.com>
2021-04-26 06:20:20 +0100

    move to new merge page

--- democlub-merge-check.user.js
+++ democlub-merge-check.user.js
@@ -1,38 +1,23 @@
 // ==UserScript==
 // @name        Democracy Club merge check
 // @namespace   sjorford@gmail.com
-// @include     https://candidates.democracyclub.org.uk/person/*/update
-// @include     https://candidates.democracyclub.org.uk/person/*/update/
-// @version     2020.10.30.1
+// @include     https://candidates.democracyclub.org.uk/person/56105/duplicate?*
+// @version     2021.04.26.0
 // @grant       none
 // ==/UserScript==
 
 $(function() {
 	
-	var mergeButton = $('#person-merge [type="submit"]').prop('disabled', true);
-	var wrapper = $('<div></div>').insertAfter(mergeButton);
-	var thisName = $('.person__hero h1').text().replace(/^Editing: /, '');
-	var otherID;
-	var inputOtherID = $('#other').on('change, keyup', event => {
-		if (inputOtherID.val() == otherID) return;
-		
-		otherID = inputOtherID.val();
-		mergeButton.prop('disabled', true);
-		wrapper.text('Checking...');
-		$.get(`/person/${otherID}`, (data, status, jqxhr) => {
-			
-			var otherName = $(data).find('.person__hero h1').text();
-			wrapper.text('Merging with ' + otherName);
-			if (otherName == thisName) {
-				mergeButton.prop('disabled', false);
-			} else {
-				wrapper.append('<div>Are you sure?<div><input type="button" class="button sjo-yesimsure" value="Yes"></div></div>')
-			}
-			
-			$('.sjo-yesimsure').click(event => mergeButton.prop('disabled', false));
-			
-		}).fail((jqxhr, status, error) => wrapper.text(status + ': ' + error));
-		
-	});
+	$('#suggestion').hide();
+	
+	var nameRow = $('td').filter((i,e) => e.innerText.trim() == 'Name').closest('tr');
+	var thisName = nameRow.find('td').eq(1).text().trim();
+	var otherName = nameRow.find('td').eq(2).text().trim();
+	
+	if (thisName != otherName) {
+		var mergeButton = $('#merge button[type="submit"]').prop('disabled', true);
+		$('<div>The primary names of the candidates are not the same - are you sure?<div><input type="button" class="button sjo-yesimsure" value="Yes"></div></div>').insertAfter('h3');
+		$('.sjo-yesimsure').click(event => mergeButton.prop('disabled', false));
+	}
 	
 });
