8dc7cc36a629e28b8b89341a0d55166660cb5c4b
sjorford <sjorford@gmail.com>
2021-12-08 08:54:48 +0000

    swap columns

--- south-tyneside-councillors-scraper.user.js
+++ south-tyneside-councillors-scraper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           South Tyneside councillors scraper
 // @namespace      sjorford@gmail.com
-// @version        2021.12.08.2
+// @version        2021.12.08.3
 // @author         Stuart Orford
 // @match          https://www.southtyneside.gov.uk/article/60209/Councillors-A-to-Z*
 // @grant          none
@@ -16,8 +16,16 @@ $(function() {
 	</style>`).appendTo('head');
 	
 	var tables = $('#COUNCILLORSLISTBYNAME_PAGE1_HTML table');
-	tables.find('tr td:nth-of-type(2), tr th:nth-of-type(2)').remove();
 	tables.find('caption').remove();
+	
+	tables.find('tr').each((i,e) => {
+		var row = $(e);
+		var nameCell  = row.find('td, th').eq(0);
+		var imageCell = row.find('td, th').eq(1);
+		var wardCell  = row.find('td, th').eq(2);
+		wardCell.insertBefore(nameCell);
+		imageCell.remove();
+	});
 	
 	var rows = tables.find('tbody tr:has(a)')
 	rows.each((i,e) => {
