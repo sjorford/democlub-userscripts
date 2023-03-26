29a7cca7083704fd835fd8332556e1c493cedd37
sjorford <sjorford@gmail.com>
2023-03-26 08:52:19 +0100

    template script

---
+++ democlub-duplicate-suggestions.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           Democracy Club duplicate suggestions
+// @namespace      sjorford@gmail.com
+// @version        2023.03.26.0
+// @author         Stuart Orford
+// @match          https://candidates.democracyclub.org.uk/duplicates/
+// @grant          none
+// @require        https://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function($) {
+$(function() {
+	
+	$(`<style>
+		
+	</style>`).appendTo('head');
+	
+});
+})(jQuery);
