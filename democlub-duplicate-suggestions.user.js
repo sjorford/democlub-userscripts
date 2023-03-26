73a58fc60c3d83ae9899892790772c5bc74d95d8
sjorford <sjorford@gmail.com>
2023-03-26 09:09:43 +0100

    compact tables, add links

--- democlub-duplicate-suggestions.user.js
+++ democlub-duplicate-suggestions.user.js
@@ -12,8 +12,24 @@
 $(function() {
 	
 	$(`<style>
-		
+		table.table table thead {display: none;}
+		table.table td {vertical-align: top;}
+		table.table tr:last-of-type td {vertical-align: middle;}
 	</style>`).appendTo('head');
+	
+	$('th:contains("Field")').closest('table.table').each((i,e) => {
+		
+		var table = $(e);
+		var idRow = $('td', table).filter((i,e) => e.innerText.trim() == "ID").closest('tr').hide();
+		var nameRow = $('td', table).filter((i,e) => e.innerText.trim() == "Name").closest('tr');
+		
+		var ids = $('td', idRow).slice(1).toArray().map(e => e.innerText.trim());
+		$('td', nameRow).slice(1).html((i,html) => `<a href="/person/${ids[i]}">${html}</a> (${ids[i]})`);
+		
+		$('td', table).filter((i,e) => e.innerText.trim() == "No other names known").text('');
+		
+	});
+	
 	
 });
 })(jQuery);
