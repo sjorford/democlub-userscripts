5757191cbd26fe1fc437c8c19b022cbae5b0dd17
sjorford <sjorford@gmail.com>
2023-04-06 13:25:35 +0100

    Update democlub-bulk-adding.user.js

--- democlub-bulk-adding.user.js
+++ democlub-bulk-adding.user.js
@@ -3,7 +3,7 @@
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/bulk_adding/sopn/*
 // @exclude     https://candidates.democracyclub.org.uk/bulk_adding/sopn/*/review/
-// @version     2022.04.01.0
+// @version     2023.04.06.0
 // @grant       none
 // @require     https://raw.githubusercontent.com/sjorford/democlub-userscripts/master/lib/utils.js
 // ==/UserScript==
@@ -49,6 +49,7 @@ function onready() {
 		.remove();
 	table.find('br').remove();
 	
+	/*
 	if (table.find('input[id$="-name"]').filter((i,e) => e.value != '').length > 0) {
 		table.find('input[id$="-name"]').filter((i,e) => e.value == '')
 			.closest('tbody').addClass('sjo-extra-row');
@@ -57,9 +58,23 @@ function onready() {
 				   && $('#sjo-show-extra-rows').hide())
 			.insertAfter(table).wrap('<div></div>');
 	}
+	*/
 	
 	// Add a checkbox for reversed names
 	$('<input type="checkbox" id="sjo-reverse" value="reverse"><label for="sjo-reverse">Surname first</label>')
 		.insertBefore(table.last()).wrapAll('<div></div>');
+	
+	var knownCandidates = $('a.not-standing').closest('table');
+	knownCandidates.find('tbody tr').appendTo(knownCandidates.first().find('tbody'));
+	knownCandidates.slice(1).hide();
+	$('.sopn_adding_table').first().before(knownCandidates);
+	
+	var parsedCandidates = $('input[id$="-DELETE"]').closest('table');
+	parsedCandidates.find('tbody tr').appendTo(parsedCandidates.first().find('tbody'));
+	parsedCandidates.slice(1).hide();
+	
+	var newCandidates = $('.sopn_adding_table').not(knownCandidates).not(parsedCandidates);
+	newCandidates.find('tbody tr').appendTo(newCandidates.first().find('tbody'));
+	newCandidates.slice(1).hide();
 	
 }
