a2026216cea045b1acdcfac3f97bb763e8ab4423
sjorford <sjorford@gmail.com>
2023-08-18 18:56:14 +0100

    Create democlub-quick-search.user.js

---
+++ democlub-quick-search.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Democracy Club quick search
+// @namespace   sjorford@gmail.com
+// @include     https://candidates.democracyclub.org.uk/*
+// @version     2023.08.18.0
+// @grant       none
+// ==/UserScript==
+
+(function($) {
+$(function() {
+	
+	$(`<style>
+		
+	</style>`).appendTo('head');
+	
+});
+})(jQuery);
