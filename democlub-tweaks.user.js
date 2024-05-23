01f90b2fbfd04c809053d7b241ef8a87965df2d4
sjorford <sjorford@gmail.com>
2024-05-23 17:40:25 +0100

    Update democlub-tweaks.user.js

--- democlub-tweaks.user.js
+++ democlub-tweaks.user.js
@@ -3,7 +3,7 @@
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/*
 // @exclude     https://candidates.democracyclub.org.uk/media/*
-// @version     2024.05.01.0
+// @version     2024.05.23.0
 // @grant       none
 // ==/UserScript==
 
