d3a5f5529ab5309271572791fc7f1175d36c3e65
Stuart Orford <sjorford@gmail.com>
2026-01-10 16:53:06 +0000

    Update democlub-candidate.user.js

--- democlub-candidate.user.js
+++ democlub-candidate.user.js
@@ -5,7 +5,7 @@
 // @exclude     https://candidates.democracyclub.org.uk/person/create/*
 // @exclude     https://candidates.democracyclub.org.uk/person/*/other-names
 // @exclude     https://candidates.democracyclub.org.uk/person/*/duplicate?*
-// @version     2024.07.06.1
+// @version     2026.01.10.0
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
 // @require     https://raw.githubusercontent.com/sjorford/democlub-userscripts/master/lib/utils.js
@@ -237,5 +237,10 @@ function onready() {
 	
 	// Disable autocomplete
 	$('#other_person_id').attr('autocomplete', 'off');
+	
+	// Hide blah
+	$('.panel').each((i,e) => {
+		if (e.innerText.trim().match(/This is .+’s entry in Democracy Club’s candidate database. These are the last details we have for .+. They may not be current. Please edit the details if you have new information./)) $(e).hide();
+	});
 	
 }
